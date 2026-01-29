ad48e3418e0238e62aa24d39d0696dbffbf8a3da
erslie <kbyk0416@gmail.com>
2026-01-29 15:53:47 +0900

    chmod ttyUSB0

--- src/01/bootload/setup.sh
+++ src/01/bootload/setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+sudo chmod 777 /dev/ttyUSB0
+
 podman run -it --rm \
   --userns=keep-id \
   -v $(pwd):/home/ersli-osdev/repo:Z \
   --device=/dev/ttyUSB0:/dev/ttyUSB0 \
   localhost/h8-dev-env /bin/bash
+  
